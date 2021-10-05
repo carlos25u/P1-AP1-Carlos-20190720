a8a8c0bd8c62ab023e2bfe0d57eb0f872f627bce
carlos lopez <carlos25u007@gmail.com>
2021-10-05 08:13:47 -0400

    Creando la entidad Aportes

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Carlos_20190720.Entidades
+{
+    class Aportes
+    {
+        [Key]
+        public int aporteID { get; set; }
+        public DateTime fechaAporte { get; set; }
+        public String persona { get; set; }
+        public String concepto { get; set; }
+        public int monto { get; set; }
+    }
+}
