113be2cac326c7e6e4cc6e0abdf098ec1b2317c1
carlos lopez <carlos25u007@gmail.com>
2021-10-05 08:29:47 -0400

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Carlos_20190720.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Carlos_20190720.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data source = DATA\Aportes.db");
+        }
+    }
+}
