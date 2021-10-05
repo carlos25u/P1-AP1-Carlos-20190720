1ea271d15bcf4f4b6afd3b35614dcfdfd3857888
carlos lopez <carlos25u007@gmail.com>
2021-10-05 08:45:45 -0400

    Creando la migracion

---
+++ Migrations/20211005124450_Migracion_inicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_AP1_Carlos_20190720.Migrations
+{
+    public partial class Migracion_inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    aporteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fechaAporte = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    persona = table.Column<string>(type: "TEXT", nullable: true),
+                    concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    monto = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.aporteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
